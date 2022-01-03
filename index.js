2f4ad834523cc098e874da62885d291853cb1f58
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-01-03 14:49:39 +0700

    add seluruh file

---
+++ index.js
@@ -0,0 +1,160 @@
+var _0x5425c7 = _0x4a69;
+(function (_0x49fa3d, _0x291847) {
+    var _0x2f410b = _0x4a69, _0x490001 = _0x49fa3d();
+    while (!![]) {
+        try {
+            var _0xf57341 = parseInt(_0x2f410b(0xd9)) / 0x1 * (-parseInt(_0x2f410b(0xdd)) / 0x2) + -parseInt(_0x2f410b(0xdf)) / 0x3 * (-parseInt(_0x2f410b(0xe9)) / 0x4) + -parseInt(_0x2f410b(0xe7)) / 0x5 + parseInt(_0x2f410b(0xea)) / 0x6 + parseInt(_0x2f410b(0xef)) / 0x7 + -parseInt(_0x2f410b(0xfa)) / 0x8 + -parseInt(_0x2f410b(0xf6)) / 0x9 * (-parseInt(_0x2f410b(0xfb)) / 0xa);
+            if (_0xf57341 === _0x291847)
+                break;
+            else
+                _0x490001['push'](_0x490001['shift']());
+        } catch (_0x39f3d8) {
+            _0x490001['push'](_0x490001['shift']());
+        }
+    }
+}(_0x5a14, 0x9c09f));
+var randomNumber = Math[_0x5425c7(0xee)](Math[_0x5425c7(0xd5)]() * 0x7), numberOfDrum = document[_0x5425c7(0xfc)](_0x5425c7(0xda))[_0x5425c7(0xed)];
+function _0x5a14() {
+    var _0x1f7b27 = [
+        'sounds/wibu/gomen_kudasai.mp3',
+        '1952170xbyCBj',
+        'querySelector',
+        '4QYLkbG',
+        '3297396xjGEUG',
+        'keydown',
+        'sounds/kick-bass.mp3',
+        'length',
+        'floor',
+        '2980586ycrUuF',
+        'key',
+        'sounds/tom-1.mp3',
+        'sounds/wibu/ara_ara.mp3',
+        'add',
+        'grey',
+        'click',
+        '2729421ePCIkp',
+        'sounds/wibu/luffy_aho_baga.mp3',
+        'footer',
+        'sounds/wibu/yahello.mp3',
+        '2511776IyekUs',
+        '30lpugea',
+        'querySelectorAll',
+        'addEventListener',
+        'by\x20Habib.',
+        'sounds/tom-3.mp3',
+        'random',
+        'sounds/tom-2.mp3',
+        'sounds/tom-4.mp3',
+        'innerHTML',
+        '1UCTTaT',
+        '.drum',
+        'style',
+        'sounds/wibu/zero_two_morning.mp3',
+        '1498106MUhkcy',
+        'color',
+        '622269XNkhjm',
+        'play',
+        'sounds/crash.mp3',
+        'remove',
+        'classList',
+        'pressed',
+        'sounds/wibu/senpai.mp3'
+    ];
+    _0x5a14 = function () {
+        return _0x1f7b27;
+    };
+    return _0x5a14();
+}
+function _0x4a69(_0x3eb129, _0x19fbb6) {
+    var _0x5a14ed = _0x5a14();
+    return _0x4a69 = function (_0x4a69ff, _0x143e43) {
+        _0x4a69ff = _0x4a69ff - 0xd5;
+        var _0x113fac = _0x5a14ed[_0x4a69ff];
+        return _0x113fac;
+    }, _0x4a69(_0x3eb129, _0x19fbb6);
+}
+for (var i = 0x0; i < numberOfDrum; i++) {
+    document[_0x5425c7(0xfc)](_0x5425c7(0xda))[i][_0x5425c7(0xfd)](_0x5425c7(0xf5), function () {
+        var _0xd035ff = _0x5425c7, _0x48be54 = this[_0xd035ff(0xd8)];
+        makeSound(_0x48be54), animationButton(_0x48be54);
+    });
+}
+document[_0x5425c7(0xfd)](_0x5425c7(0xeb), function (_0x160092) {
+    var _0x3575ab = _0x5425c7;
+    makeSound(_0x160092[_0x3575ab(0xf0)]), animationButton(_0x160092[_0x3575ab(0xf0)]);
+});
+function makeSound(_0x4aa825) {
+    var _0x2f7dc3 = _0x5425c7;
+    switch (_0x4aa825) {
+    case 'm':
+        var _0x10cb7c = new Audio(_0x2f7dc3(0xf2));
+        _0x10cb7c['play']();
+        break;
+    case 'n':
+        var _0x156a8d = new Audio(_0x2f7dc3(0xe6));
+        _0x156a8d[_0x2f7dc3(0xe0)]();
+        break;
+    case 'o':
+        var _0x2f595a = new Audio(_0x2f7dc3(0xf7));
+        _0x2f595a['play']();
+        break;
+    case 'p':
+        var _0x63b4a4 = new Audio('sounds/wibu/nyah_arigato_nya.mp3');
+        _0x63b4a4[_0x2f7dc3(0xe0)]();
+        break;
+    case 'b':
+        var _0x4af74b = new Audio('sounds/wibu/oi_oi_oi_bakugo.mp3');
+        _0x4af74b[_0x2f7dc3(0xe0)]();
+        break;
+    case 'v':
+        var _0x29623d = new Audio(_0x2f7dc3(0xe5));
+        _0x29623d[_0x2f7dc3(0xe0)]();
+        break;
+    case 'c':
+        var _0x1bfd78 = new Audio(_0x2f7dc3(0xf9));
+        _0x1bfd78[_0x2f7dc3(0xe0)]();
+        break;
+    case 't':
+        var _0x4d0987 = new Audio(_0x2f7dc3(0xdc));
+        _0x4d0987['play']();
+        break;
+    case 'w':
+        var _0x26f267 = new Audio(_0x2f7dc3(0xe1));
+        _0x26f267[_0x2f7dc3(0xe0)]();
+        break;
+    case 'a':
+        var _0x4b8269 = new Audio(_0x2f7dc3(0xec));
+        _0x4b8269[_0x2f7dc3(0xe0)]();
+        break;
+    case 's':
+        var _0x3ce2ac = new Audio('sounds/snare.mp3');
+        _0x3ce2ac[_0x2f7dc3(0xe0)]();
+        break;
+    case 'd':
+        var _0x3f83ba = new Audio(_0x2f7dc3(0xf1));
+        _0x3f83ba[_0x2f7dc3(0xe0)]();
+        break;
+    case 'j':
+        var _0x2d6206 = new Audio(_0x2f7dc3(0xd6));
+        _0x2d6206['play']();
+        break;
+    case 'k':
+        var _0xbf73d0 = new Audio(_0x2f7dc3(0xff));
+        _0xbf73d0['play']();
+        break;
+    case 'l':
+        var _0x289718 = new Audio(_0x2f7dc3(0xd7));
+        _0x289718[_0x2f7dc3(0xe0)]();
+        break;
+    default:
+        break;
+    }
+}
+function animationButton(_0x3b2525) {
+    var _0xe2562f = _0x5425c7, _0x5755e9 = document[_0xe2562f(0xe8)]('.' + _0x3b2525);
+    _0x5755e9['classList'][_0xe2562f(0xf3)]('pressed'), setTimeout(() => {
+        var _0x4630bd = _0xe2562f;
+        _0x5755e9[_0x4630bd(0xe3)][_0x4630bd(0xe2)](_0x4630bd(0xe4));
+    }, 0x3e8);
+}
+document[_0x5425c7(0xe8)](_0x5425c7(0xf8))[_0x5425c7(0xd8)] = _0x5425c7(0xfe), document[_0x5425c7(0xe8)](_0x5425c7(0xf8))[_0x5425c7(0xdb)][_0x5425c7(0xde)] = _0x5425c7(0xf4);
